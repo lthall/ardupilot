7481747e02f73c02786e29e1cdddcf365acca8b6
Leonard Hall <leonardthall@gmail.com>
2018-10-04 00:46:27 +0930

    Updates

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V3.10.0-xcraft4"
+#define THISFIRMWARE "ArduPlane V3.10.0-xcraft7"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 3,10,0,FIRMWARE_VERSION_TYPE_DEV
