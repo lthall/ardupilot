7917caf03371d240fa03d92ff78b7e68c9db972e
Andy Piper <github@andypiper.com>
2019-06-17 09:43:08 +0100

    Filter: New HarmonicNotchFilter that delegates updates to a bank of NotchFilters located at an rpm frequency and harmonics. Center frequency can be updated dynamically. Notch parameters are configurable, including the number of harmonics to filter on. Updates to the filter parameters are optimized across the notch bank.

---
+++ libraries/Filter/HarmonicNotchFilter.h
@@ -0,0 +1,59 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <AP_Math/AP_Math.h>
+#include <cmath>
+#include <inttypes.h>
+#include <AP_Param/AP_Param.h>
+#include "NotchFilter.h"
+
+
+template <class T>
+class HarmonicNotchFilter {
+public:
+    // set parameters
+    void create(uint8_t harmonics);
+    void init(float sample_freq_hz, float center_freq_hz, float bandwidth_hz, float attenuation_dB);
+    T apply(const T &sample);
+    void reset();
+    void update(float center_freq_hz);
+
+private:
+    NotchFilter<T>*  filters;
+    float sample_freq_hz;
+    float bandwidth_hz;
+    float attenuation_dB;
+    uint8_t harmonics;
+    bool initialised;
+};
+
+/*
+  notch filter enable and filter parameters
+ */
+class HarmonicNotchFilterParams : public NotchFilterParams {
+public:
+    HarmonicNotchFilterParams(void);
+    void set_center_freq_hz(uint16_t center_freq_hz) { _center_freq_hz.set(center_freq_hz); }
+    uint8_t harmonics(void) const { return _harmonics; }
+    static const struct AP_Param::GroupInfo var_info[];
+
+private:
+    AP_Int8 _harmonics;
+};
+
+typedef HarmonicNotchFilter<float> HarmonicNotchFilterFloat;
+typedef HarmonicNotchFilter<Vector3f> HarmonicNotchFilterVector3f;
+
