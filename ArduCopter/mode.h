48a35a57325e952ed9704bc90e128153b6f819ef
Randy Mackay <rmackay9@yahoo.com>
2020-01-08 20:19:17 +0900

    Copter: wp_start provides next_dest_loc
    also send next_destination to wp_nav instead of setting fast_waypoint

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -351,9 +351,8 @@ public:
     bool loiter_start();
     void rtl_start();
     void takeoff_start(const Location& dest_loc);
-    void wp_start(const Vector3f& destination, bool terrain_alt);
     void wp_start(const Location& dest_loc);
-    void wp_next(const Location& dest_loc);
+    void wp_start(const Location& dest_loc, const Location& next_dest_loc);
     void land_start();
     void land_start(const Vector3f& destination);
     void circle_movetoedge_start(const Location &circle_center, float radius_m);
