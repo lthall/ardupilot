52b268700e0158475d42fd33af3f9229df0ba26d
Leonard Hall <leonardthall@gmail.com>
2020-05-10 17:19:09 +0930

    Copter: Version AK1

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.3"
+#define THISFIRMWARE "ArduCopter V4.0.3.AK1"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,3,FIRMWARE_VERSION_TYPE_OFFICIAL
