9c549bac414cf51f72569d6dbb1d84bae37f69d9
Randy Mackay <rmackay9@yahoo.com>
2020-07-15 10:39:50 +0900

    Copter: Guided takeoff does not reset target

--- ArduCopter/mode_guided.cpp
+++ ArduCopter/mode_guided.cpp
@@ -396,8 +396,8 @@ void ModeGuided::takeoff_run()
         copter.landinggear.retract_after_takeoff();
 
         // switch to position control mode but maintain current target
-        const Vector3f target = wp_nav->get_wp_destination();
-        set_destination(target, false, 0, false, 0, false, wp_nav->origin_and_destination_are_terrain_alt());
+        //const Vector3f target = wp_nav->get_wp_destination();
+        //set_destination(target, false, 0, false, 0, false, wp_nav->origin_and_destination_are_terrain_alt());
     }
 }
 
