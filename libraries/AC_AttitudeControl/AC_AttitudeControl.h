ba30f773622de59976cf6d308ed46b597cbb7ef9
Leonard Hall <leonardthall@gmail.com>
2021-02-02 02:11:23 +1030

    AC_AttitudeControl: Support higher lean angles

--- libraries/AC_AttitudeControl/AC_AttitudeControl.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl.h
@@ -33,7 +33,7 @@
 #define AC_ATTITUDE_CONTROL_RATE_BF_FF_DEFAULT          1       // body-frame rate feedforward enabled by default
 
 #define AC_ATTITUDE_CONTROL_ANGLE_LIMIT_TC_DEFAULT      1.0f    // Time constant used to limit lean angle so that vehicle does not lose altitude
-#define AC_ATTITUDE_CONTROL_ANGLE_LIMIT_THROTTLE_MAX    0.8f    // Max throttle used to limit lean angle so that vehicle does not lose altitude
+#define AC_ATTITUDE_CONTROL_ANGLE_LIMIT_THROTTLE_MAX    0.95f    // Max throttle used to limit lean angle so that vehicle does not lose altitude
 #define AC_ATTITUDE_CONTROL_ANGLE_LIMIT_MIN             10.0f   // Min lean angle so that vehicle can maintain limited control
 
 #define AC_ATTITUDE_CONTROL_MIN_DEFAULT                 0.1f    // minimum throttle mix default
