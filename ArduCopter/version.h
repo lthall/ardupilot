aeccc230ca428ac48528d59537ee7e3f235e25ef
Leonard Hall <leonardthall@gmail.com>
2021-02-09 00:16:29 +1030

    Copter: Version V4.0.6-revD

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.6-BSTB"
+#define THISFIRMWARE "ArduCopter V4.0.6-revD"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,6,FIRMWARE_VERSION_TYPE_OFFICIAL
