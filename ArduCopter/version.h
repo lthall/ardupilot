8eb3031b395be0c2e9a2ed39b1eb67f5101f6d64
Leonard Hall <leonardthall@gmail.com>
2021-03-31 02:08:28 +1030

    Copter: Version J

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.0.4-revI"
+#define THISFIRMWARE "ArduCopter V4.0.4-revJ"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,0,4,FIRMWARE_VERSION_TYPE_OFFICIAL
