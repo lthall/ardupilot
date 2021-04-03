d276bbd4e0297e989e697ec9fd28c83b36c41b41
Leonard Hall <leonardthall@gmail.com>
2021-04-04 02:16:17 +1030

    Copter: Version V4.1.0-devM

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.1.0-devL+AH"
+#define THISFIRMWARE "ArduCopter V4.1.0-devM"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,1,0,FIRMWARE_VERSION_TYPE_DEV
